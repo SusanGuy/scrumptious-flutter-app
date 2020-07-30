0606a489f5f507f7599e4d4f01b79dfdb39ece18
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-07-30 18:21:10 -0500

    Done with the meal item widget

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -14,6 +14,39 @@ class MealItem extends StatelessWidget {
       @required this.duration,
       @required this.complexity,
       @required this.affordability});
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+
+      case Complexity.Challenging:
+        return "Challenging";
+
+      case Complexity.Hard:
+        return "Hard";
+
+      default:
+        return "Unknown";
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+
+      case Affordability.Luxurious:
+        return "Luxurious";
+
+      case Affordability.Pricey:
+        return "Pricey";
+
+      default:
+        return "Unknown";
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -34,7 +67,52 @@ class MealItem extends StatelessWidget {
                 child: Image.network(imageUrl,
                     height: 250, width: double.infinity, fit: BoxFit.cover),
               ),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  width: 220,
+                  color: Colors.black54,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Text(
+                    title,
+                    style: TextStyle(fontSize: 26, color: Colors.white),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
             ]),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Icon(Icons.schedule),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text('$duration min'),
+                  ]),
+                  Row(children: <Widget>[
+                    Icon(Icons.work),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text(complexityText),
+                  ]),
+                  Row(children: <Widget>[
+                    Icon(Icons.attach_money),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text(affordabilityText),
+                  ]),
+                ],
+              ),
+            ),
           ])),
     );
   }
